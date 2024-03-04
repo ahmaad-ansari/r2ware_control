1d49c17afeaa4be8a10e6556a688cf2e93257032
Ahmaad Ansari <88805493+ahmaad-ansari@users.noreply.github.com>
2024-03-03 23:24:43 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# r2ware_control
+
+```
+ros2 launch r2ware_control r2ware_control_launch.py 
+
+```
---
+++ launch/r2ware_control_launch.py
@@ -0,0 +1,21 @@
+import os
+
+from ament_index_python import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='r2ware_control',
+            executable='r2ware_control_node',
+            output='screen',
+            prefix=[],
+        ),
+    ])
